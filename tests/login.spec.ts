2a2dcd02de27163ebf400cb922e91a4f8fc449f0
RoszkowskiM <m.roszkowski88@gmail.com>
2026-02-02 21:08:29 +0100

    added more tags & annotations

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -37,7 +37,7 @@ test.describe('User login to Demobank', () => {
 
   test(
     'unsuccessful login with too short username',
-    { tag: '@login' },
+    { tag: ['@login', '@unhappy_path'] },
     async ({ page }) => {
       // Arrange
       const invalidUserId = 'tester';
@@ -54,7 +54,7 @@ test.describe('User login to Demobank', () => {
 
   test(
     'unsuccessful login with too short password',
-    { tag: '@login' },
+    { tag: ['@login', '@unhappy_path'] },
     async ({ page }) => {
       // Arrange
       const userId = loginData.userId;
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -20,7 +20,13 @@ test.describe('Pulpit tests', () => {
 
   test(
     'quick payment with correct data',
-    { tag: ['@pulpit', '@integration'] },
+    {
+      tag: ['@pulpit', '@integration'],
+      annotation: {
+        type: 'Documentation',
+        description: 'https://jaktestowac.pl/course/playwright-wprowadzenie/',
+      },
+    },
     async ({ page }) => {
       // Arrange
       const recieverId = '2';
